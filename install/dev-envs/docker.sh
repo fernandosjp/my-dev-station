caedeaf6a502c77390cfe19d9061af393dcdaa88
Fernando <fernandosjp@gmail.com>
2025-10-01 21:03:25 -0300

    Fix docker installation to avoid installing Docker desktop

--- install/dev-envs/docker.sh
+++ install/dev-envs/docker.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-
-# Install Docker engine and standard plugins
-brew install --cask docker
+#
+# Install docker colima to keep the stack opensource and license free
+brew install docker colima
+# Start service in background
+brew services start colima
 
 # Limit log size to avoid running out of disk
 echo '{"log-driver":"json-file","log-opts":{"max-size":"10m","max-file":"5"}}' | sudo tee /etc/docker/daemon.json
